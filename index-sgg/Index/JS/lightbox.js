52081cfdb0a8cb5af5bca5150cfe0a9aad6f1003
Yassirus99 <78995475+Yassirus99@users.noreply.github.com>
2022-05-14 18:51:53 +0000

    Add files via upload

---
+++ index-sgg/Index/JS/lightbox.js
@@ -0,0 +1,31 @@
+const imagenes = document.querySelectorAll('.img-galeria')
+const imagenesLight = document.querySelector('.agregar-imagen')
+const contenedorLight = document.querySelector('.imagen-light')
+const inicio1 = document.querySelector('.inicio');
+
+console.log(imagenes)
+console.log(imagenesLight)
+console.log(contenedorLight)
+
+imagenes.forEach(imagen => {
+    imagen.addEventListener('click', () => {
+        aparecerImagen(imagen.getAttribute('src'))
+    })
+})
+
+contenedorLight.addEventListener('click', (e) => {
+    if (e.target !== imagenesLight) {
+
+        contenedorLight.classList.toggle('show')
+        imagenesLight.classList.toggle('showImage')
+        inicio1.style.opacity = '1'
+
+    }
+})
+
+const aparecerImagen = (imagen) => {
+    imagenesLight.src = imagen;
+    contenedorLight.classList.toggle('show')
+    imagenesLight.classList.toggle('showImage')
+    inicio1.style.opacity = '0'
+}
---
+++ index-sgg/README.md
@@ -0,0 +1,2 @@
+# examples-from-pages
+son paginas de ejemplos creadas a partir de turoriales, bastante basicas
